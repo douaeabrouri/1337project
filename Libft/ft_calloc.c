36ec4d411ecaf364aefec0cbfa3dd02e126eed00
Douaeoxo <douaeoxo@asus>
2025-08-30 01:47:08 +0100

    ...

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -1,1 +1,27 @@
-#include
+#include <stdio.h>
+#include <stlib.h>
+
+void ft_bzero(void *str, size_t size)
+{
+    unsigned char *s;
+    size_t index;
+
+    s = (unsigned char *)str;
+    index = 0;
+    while(index < size)
+    {
+        s[index] = 0;
+        index++;
+    }
+}
+
+void *ft_calloc(size_t nmb, size_t size)
+{
+    void *tmp;
+    
+    tmp = malloc(size * nmb + 1);
+    if(tmp == NULL)
+        return(NULL);
+    ft_bzero(tmp, nmb * size);
+    return(tmp);
+}
---
+++ Libft/ft_strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int len(const char *str)
+{
+    int index;
+
+    index = 0;
+    while(str[index])
+        index++;
+    return (index);
+}
+
+char *ft_strdup(char *string)
+{
+    char *tmp;
+    int index;
+
+    tmp = (char *)malloc(sizeof(char) * len(string));
+    if(tmp == NULL)
+    {
+        return (NULL);
+    }
+    index = 0;
+    while(string[index])
+    {
+        tmp[index] = string[index];
+        index++;
+    }
+    tmp[index] = '\0';
+    return (tmp);
+}
+
+int main(void)
+{
+    printf("%s\n",ft_strdup("oxo"));
+}
