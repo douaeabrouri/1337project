39c32c1c58906c4712734e140bc631d87a0392ab
Douaeoxo <douaeoxo@asus>
2025-09-26 14:15:26 +0100

    ...

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -2,22 +2,28 @@
 
 void ft_putnbr_fd(int n, int fd)
 {
-    int index;
+    char c;
 
-    index = 0;
+
+    if (n == -2147483648)
+    {
+        write(fd, "-2147483648", 11);
+        return;
+    }
+
     if(nb < 0)
     {
         write(fd, '-', 1);
         nb *= (-1);
     }
-    else if(nb >= 0 && nb <= 9)
+    if(nb >= 0 && nb <= 9)
     {
         nb += '0'; 
         write(fd, nb, 1);
     }
-    else if(nb >= 10)
-    {
-        ft_putnbr_fd(nb / 10):
-        write(fd, ((nb % 10) + '0'), 1):
-    }
+    if(nb >= 10)
+        ft_putnbr_fd(nb / 10);
+    c = (nb % 10) + '0';
+    write(fd, &c, 1);
+
 }
