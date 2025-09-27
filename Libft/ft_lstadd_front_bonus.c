3a2aab1f4290ebd62cd2bccafe7716ce9005e084
Douaeoxo <douaeoxo@asus>
2025-09-27 14:12:46 +0100

    ...

---
+++ Libft/ft_lstadd_front_bonus.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+typedef struct s_list
+{
+    void    *content;
+    struct s_list   *next;
+}   t_list;
+
+void    ft_lstadd_front(t_list **lst, t_list *new)
+{
+    if(lst && new)
+    {
+        new -> next = (*lst);
+        (*lst) = new;
+    }
+    
+}
